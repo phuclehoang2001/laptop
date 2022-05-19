769ea4ebdf168554ada00c8766b99adb5d688da2
lyphuc823 <75345469+lyphuc823@users.noreply.github.com>
2022-05-19 20:36:43 +0700

    taikhoan GD form

--- admin/giaodien/taikhoan_adding.php
+++ admin/giaodien/taikhoan_adding.php
@@ -1,23 +1,35 @@
-<h1>Thêm tài khoản</h1>
-<form name="taikhoan-formadd" method="POST" action="./xulythem.php" enctype="multipart/form-data">
-    
-    <div class="clear-both"></div>
+<style>
+    .wrap-field li {
+        list-style: none;
+    }
+</style>
+<h1 style="text-align: center;">Thêm tài khoản</h1>
+<form name="taikhoan-formadd" class="form-group" method="POST" action="./xulythem.php" enctype="multipart/form-data">
+
+    <!-- <div class="clear-both"></div> -->
     <div class="box-content">
-    <div class="wrap-field">
-        <label>Tài khoản: </label>
-        <input type="text" name="tendangnhap" value="" />
-        <div class="clear-both"></div>
-    </div>
-    <div class="wrap-field">
-        <label>Mật khẩu: </label>
-        <input type="text" name="matkhau" value="" />
-        <div class="clear-both"></div>
-    </div>
-    <div class="wrap-field">
-        <label>Họ và tên: </label>
-        <input type="text" name="name" value="" />
-        <div class="clear-both"></div>
-    </div>
-    <input name="btntkadd" type="submit" title="Lưu tài khoản" value="Lưu" />
+        <div class="modal-header">
+            <h5 class="modal-title">Chỉnh sửa</h5>
+        </div>
+        <div class="modal-body">
+            <div class="wrap-field">
+                <label>Tài khoản: </label>
+                <input class="form-control" type="text" name="tendangnhap" value="" />
+                <div class="clear-both"></div>
+            </div>
+            <div class="wrap-field">
+                <label>Mật khẩu: </label>
+                <input class="form-control"type="text" name="matkhau" value="" />
+                <div class="clear-both"></div>
+            </div>
+            <div class="wrap-field">
+                <label>Họ và tên: </label>
+                <input class="form-control"type="text" name="name" value="" />
+                <div class="clear-both"></div>
+            </div>
+        </div>
+        <div class="modal-footer">
+        <input name="btntkadd" class="btn btn-primary"type="submit" title="Lưu tài khoản" value="Lưu" />
+        </div>
     </div>
 </form>
