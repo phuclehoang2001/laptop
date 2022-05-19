4cdc8afbf185f92a80359d5e8466d73a02e8b5b6
lyphuc823 <75345469+lyphuc823@users.noreply.github.com>
2022-05-19 17:51:15 +0700

    Update pagination.php
    Sau khi sắp xếp qua trang mới vẫn được sắp xếp

--- admin/pagination.php
+++ admin/pagination.php
@@ -3,19 +3,19 @@
 if ($current_page > 3) {
     $first_page = 1;
     ?>
-    <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $first_page ?>">First</a></li>
+    <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $first_page ?><?php if(isset($_GET['sapxep']))echo '&sapxep='.$_GET['sapxep']?>">First</a></li>
     <?php
 }
 if ($current_page > 1) {
     $prev_page = $current_page - 1;
     ?>
-    <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $prev_page ?>">Prev</a></li>
+    <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $prev_page ?><?php if(isset($_GET['sapxep']))echo '&sapxep='.$_GET['sapxep']?>">Prev</a></li>
 <?php }
 ?>
 <?php for ($num = 1; $num <= $totalPages; $num++) { ?>
     <?php if ($num != $current_page) { ?>
         <?php if ($num > $current_page - 3 && $num < $current_page + 3) { ?>
-            <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $num ?>"><?= $num ?></a></li>
+            <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $num ?><?php if(isset($_GET['sapxep']))echo '&sapxep='.$_GET['sapxep']?>"><?= $num ?></a></li>
         <?php } ?>
     <?php } else { ?>
         <li class="page-item"><strong class="current-page page-link"><?= $num ?></strong></li>
@@ -25,13 +25,13 @@ if ($current_page > 1) {
 if ($current_page < $totalPages - 1) {
     $next_page = $current_page + 1;
     ?>
-    <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $next_page ?>">Next</a></li>
+    <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $next_page ?><?php if(isset($_GET['sapxep']))echo '&sapxep='.$_GET['sapxep']?>">Next</a></li>
 <?php
 }
 if ($current_page < $totalPages - 3) {
     $end_page = $totalPages;
     ?>
-    <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $end_page ?>">Last</a></li>
+    <li class="page-item"><a class="page-link" href="admin.php?tmuc=<?= $_GET['tmuc'] ?>&per_page=<?= $item_per_page ?>&page=<?= $end_page ?><?php if(isset($_GET['sapxep']))echo '&sapxep='.$_GET['sapxep']?>">Last</a></li>
     <?php
 }
 ?>
